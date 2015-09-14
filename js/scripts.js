5b9e21f6b615e88b762fb2763f089f59a7f45592
Vaughn Dice & Janice Dunn <student@epicodus.com>
2015-09-14 11:42:05 -0700

    Add name, location, date, and notes of places.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
 
     var newPlaceName = $("input#new-place-name").val();
     var newPlaceLocation = $("input#new-place-location").val();
+    var newPlaceDate = $("input#new-place-date").val();
     var newPlaceNotes = $("textarea#new-place-notes").val();
-    var newPlace = { name: newPlaceName, location: newPlaceLocation, notes: newPlaceNotes };
+    var newPlace = { name: newPlaceName, location: newPlaceLocation, date: newPlaceDate, notes: newPlaceNotes };
 
     $('ul#places').append('<li><span class="place">' + newPlace.name + "</span></li>")
 
@@ -14,11 +15,13 @@ $(document).ready(function() {
       $("#show-place h2").text(newPlace.name);
       $(".place-name").text(newPlace.name);
       $(".place-location").text(newPlace.location);
+      $(".place-date").text(newPlace.date);
       $(".place-notes").text(newPlace.notes);
     });
 
     $("input#new-place-name").val("");
     $("input#new-place-location").val("");
+    $("input#new-place-date").val("");
     $("input#new-place-notes").val("");
 
   });
